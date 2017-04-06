7d0ca89706e835f53e9f8163bffc3f856d95e13d
QAZXWS <qazwyt3196@gmail.com>
2017-04-06 22:14:53 +0800

    clean UP for demo

--- single_cycle/testcase/test.s
+++ single_cycle/testcase/test.s
@@ -1,44 +1,38 @@
 |   PC = 0x00000008 																		#check if initial PC ok
-|
-|
-|
-|
-|
-|
 |#test largest positive - second smallest negative OVF
 |	lw $1,0x0000($0)                                                                        #pc=0x13C
 |	lw $2,0x0010($0)                                                                        #pc=0x140
 |	sub $1,$1,$2                                                                            #pc=0x144
-|#test addi largest ovf 
+|#test addi largest ovf
 |	lw $1,0x0000($0)																		#pc=0x148
 |	addi $1,$1,0x7FFF                                                                       #pc=0x14C
-|#test addi smallest ovf 	
+|#test addi smallest ovf
 |	lw $1,0x0008($0)																		#pc=0x150
 |   addi $1,$1,0x8000                                                                       #pc=0x154
 |#test largest positive sub smallest negative overflow
 |   lw $1,0x0000($0)                                 										#pc=0x158
 |   lw $2,0x0008($0)                                 										#pc=0x15C
 |   sub $1,$1,$2                                     										#pc=0x160
-|#test largest positive sub overflow                 										
+|#test largest positive sub overflow
 |   lw $1,0x0000($0)                                 										#pc=0x164
 |   lw $2,0x0000($0)                                 										#pc=0x168
 |   sub $1,$1,$2                                                                            #pc=0x16C
-|#test smallest negative sub overflow                                                       
+|#test smallest negative sub overflow
 |   lw $1,0x0008($0)                                                                        #pc=0x170
 |   lw $2,0x0008($0)                                                                        #pc=0x174
 |   sub $1,$1,$2                                                                            #pc=0x178
-|#test smallest negative add overflow                                                      
+|#test smallest negative add overflow
 |   lw $1,0x0008($0)                                                                        #pc=0x17C
 |   lw $2,0x0008($0)                                                                        #pc=0x180
 |   add $1,$1,$2                                                                            #pc=0x184
-|#test biggest positive add overflow                                                        
+|#test biggest positive add overflow
 |   lw $1,0($0)                                                                             #pc=0x188
 |   lw $2,0($0)                                                                             #pc=0x18C
 |   add $1,$1,$2                                                                            #pc=0x190
-|#test smallest negative sub largest positive overflow                                      
+|#test smallest negative sub largest positive overflow
 |   lui $1, 0x8000                                                                          #pc=0x194
 |   sub $2,$1,$2                                                                            #pc=0x198
-|#test addi overflow                                                                        
+|#test addi overflow
 |   addi $2,$1,0x8000                                                                       #pc=0x19C
 |#test only no write $0 error
 |   sll $0,$0,0 																			#pc=0x1A0
@@ -58,17 +52,17 @@
 |   lw $4,0($0) 																			#pc=0x1C4
 |   sw $0,0xff01($3) 																		#pc=0x1C8
 |   lw $4,0($0)                                                                             #pc=0x1CC
-|#test save halfword                                                                        
+|#test save halfword
 |   sh $1,0xff01($3)                                                                        #pc=0x1D0
 |   lw $4,0($0)                                                                             #pc=0x1D8
-|#test save byte                                                                            
+|#test save byte
 |   sb $0,0xff02($3)                                                                        #pc=0x1DC
 |   lw $4,0($0)                                                                             #pc=0x1E0
-|#test unsigned I-command                                                                   
+|#test unsigned I-command
 |   andi $2,$1,0xffff                                                                       #pc=0x1E4
 |   ori $2,$2,0xffff                                                                        #pc=0x1E8
 |   nori $2,$2,0xffff                                                                       #pc=0x1EC
-|#test signed comparison                                                                    
+|#test signed comparison
 |   slti $2,$1,0xffff                                                                       #pc=0x1F0
 |   slti $2,$1,0x8000                                                                       #pc=0x1F4
 |   slti $2,$0,0xffff                                                                       #pc=0x1F8
@@ -81,33 +75,33 @@
 |   halt                                    												#pc=0x20C
 |   halt                                    												#pc=0x210
 |	#test every writeto0
-|   add $0,$0,$0                            												#pc=0x214
-|   addu $0,$0,$0                           												#pc=0x218
-|   sub $0,$0,$0                            												#pc=0x21C
-|   and $0,$0,$0                            												#pc=0x220
-|   or $0,$0,$0                             												#pc=0x224
-|   xor $0,$0,$0                            												#pc=0x228
-|   nor $0,$0,$0                            												#pc=0x22C
-|	nand $0,$0,$0																			#pc=0x230
-|	slt $0,$0,$0                                                                            #pc=0x234
-|	srl $0,$0,0X0000                                                                        #pc=0x238
-|	sra $0,$0,0X0000                                                                        #pc=0x23C
-|	mult $1,$1                                                                              #pc=0x240
-|	multu $1,$1                                                                             #pc=0x244
-|	mfhi $0																					#pc=0x248
-|	mult $1,$1                                                                              #pc=0x24C
-|	addi  $0,$1,0xffff                                                                      #pc=0x250
-|	addiu  $0,$1,0xffff                                                                     #pc=0x254
-|	lw   $0,0x0000($0)                                                                      #pc=0x258
-|	lh    $0,0x0000($0)                                                                     #pc=0x25C
-|	lhu   $0,0x0000($0)                                                               		#pc=0x260
-|	lb    $0,0x0000($0)                                                                 	#pc=0x264
-|	lbu	  $0,0x0000($0)																		#pc=0x268
-|   lui   $0,0x0000                                                                         #pc=0x26C
-|	andi   $0,$1,0xffff                                                                     #pc=0x270
-|	ori    $0,$1,0xffff                                                                     #pc=0x274
-|	nori   $0,$1,0xffff                                                                     #pc=0x278
-|	slti    $0,$1,0xffff                                                                    #pc=0x27C   
+|   add $0,$0,$0
+|   addu $0,$0,$0
+|   sub $0,$0,$0
+|   and $0,$0,$0
+|   or $0,$0,$0
+|   xor $0,$0,$0
+|   nor $0,$0,$0
+|	nand $0,$0,$0
+|	slt $0,$0,$0
+|	srl $0,$0,0X0000
+|	sra $0,$0,0X0000
+|	mult $1,$1
+|	multu $1,$1
+|	mfhi $0																					
+|	mult $1,$1
+|	addi  $0,$1,0xffff
+|	addiu  $0,$1,0xffff
+|	lw   $0,0x0000($0)
+|	lh    $0,0x0000($0)
+|	lhu   $0,0x0000($0)
+|	lb    $0,0x0000($0)
+|	lbu	  $0,0x0000($0)
+| lui   $0,0x0000
+|	andi   $0,$1,0xffff
+|	ori    $0,$1,0xffff
+|	nori   $0,$1,0xffff
+|	slti    $0,$1,0xffff
 |	#test branch
 |   addi $2,$0,0x0000                                                                       #pc=0x280
 ||<-beq  $2,$0,0x0001                                                                       #pc=0x284
@@ -132,19 +126,3 @@
 .
 |
 |------ j 0x200  #pc=0x3fc command = 0x08000080 jump to PC=0x200							#pc=0x3FC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
